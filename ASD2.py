2197566b872652cb1a85bec07e3231f25fb2bad7
DivyaAntonyThazhekkadan <125890593+DivyaAntonyThazhekkadan@users.noreply.github.com>
2023-04-02 13:33:26 +0100

    Add files via upload

---
+++ ASD2.py
@@ -0,0 +1,108 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 23 13:39:06 2023
+
+@author: user
+"""
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def fn_read_Excel(fileName):
+    """
+    
+
+    Parameters
+    ----------
+    fileName : TYPE - string
+        DESCRIPTION. - excelfile name + the path
+
+    Returns
+    -------
+    dataframes one with years as column and one with countries as columns
+
+    """
+    df = pd.read_excel(fileName)
+    # print(df)
+    list_drop_columns=['Country Code','Indicator Name','Indicator Code']
+    df.drop(labels='Country Code',axis=1,inplace=True)
+    df.drop(labels='Indicator Name',axis=1,inplace=True)
+    df.drop(labels='Indicator Code',axis=1,inplace=True)
+    # print(df)
+    list_Selected_Countries = ['Australia','Japan','United Kingdom',
+                                'United Arab Emirates','Canada','United States',
+                                'China','Georgia','Brazil','India','Zimbabwe',
+                                'Yemen','Sudan']
+    # select data for the selected Coutries
+    df_SCountries=df.loc[df['Country Name'].isin(list_Selected_Countries)]
+    # print(df_SCountries)
+    #remove columns with no data
+    df_SCountries.dropna(axis=1,how='all', thresh=None, subset=None, \
+                          inplace=True)
+    df_SCountries.set_index('Country Name')
+    
+    df_Transpose=df_SCountries.transpose()
+    
+    return df_SCountries,df_Transpose     
+
+def BarPlot(df,xlbl='',ylbl='',title='',filename=''):
+    """   
+    Parameters
+    ----------
+    df : TYPE
+        DESCRIPTION.-- plots a grouped bar plot based on df
+    Returns
+    -------
+    None.
+
+    """
+    filepath='C:\\Divya UH Academics\\Divya Canvas\\ADS\\Assignments\\April 2nd- Assignment 2\\'
+    #lst_years=['1990','1995','2000','2005','2010','2015']
+    lst_Rqurd_Colms = ['Country Name','1990','1995','2000','2005','2010','2015']
+    df_plot = df[df.columns[df.columns.isin(lst_Rqurd_Colms)]]
+   
+    df_plot.set_index('Country Name',inplace=True)
+    df_plot.replace('United Arab Emirates','UAE',inplace = True,regex = True)
+    df_plot.replace('United Kingdom','UK',inplace = True,regex = True)    
+    #plot bar graph,
+    df_plot.plot(kind='bar',title=title,xlabel=xlbl,ylabel=ylbl)
+    # if(xlbl!=''):
+    #     plt.xlabel(xlabel=xlbl,fontsize=20)
+    # if(ylbl!=''):
+    #     plt.ylabel(ylabel=ylbl,fontsize=20)
+    # plt.title(title,fontsize=20)
+    
+    # plt.legend(fontsize='10',loc='upper right')
+    # plt.xticks(rotation=90)
+    plt.savefig(filepath+filename+".png",bbox_inches = "tight",dpi=100)
+    plt.show()
+
+def LinePlot(df):
+    """   
+
+    Parameters
+    ----------
+    df : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    None.
+
+    """
+
+
+#read co2 emission data file
+filename ='API_EN.ATM.CO2E.KT_DS2_en_excel_v2_5178991.xlsx'
+filepath='C:\\Divya UH Academics\\Divya Canvas\\ADS\\Assignments\\April 2nd- Assignment 2\\'
+file=filepath+filename
+df_CO2_CountryWise,df_CO2_Yearwise = fn_read_Excel(file)
+# print(df_CO2_CountryWise)
+# print(df_CO2_Yearwise)
+print(df_CO2_CountryWise.describe())
+
+BarPlot(df_CO2_CountryWise,'Country Name','CO2 emissions (kt)','Carbon dioxide emissions Summary','co2 bar')
+
+   
+    
